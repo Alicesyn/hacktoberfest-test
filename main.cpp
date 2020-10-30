9f209fec7fdb7c1369899a35188ce2a168b06608
Alicesyn <quynhanhtvu@gmail.com>
2020-10-30 03:20:26 -0500

    add main class

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adagaki on 10/30/2020.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Adagaki on 10/30/2020.
+//
+
+#ifndef HACKTOBERFEST_TEST_MAIN_H
+#define HACKTOBERFEST_TEST_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //HACKTOBERFEST_TEST_MAIN_H
